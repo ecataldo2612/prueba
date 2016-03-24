3b61a9f050757db5b4c30959716f1c88935f85cd
ecataldo2612 <ecataldo@gmail.com>
2016-03-24 16:43:23 -0300

    commit

---
+++ ejemploSpring/src/com/ejemplo/controller/ejemploHolaMundo.java
@@ -0,0 +1,22 @@
+package com.ejemplo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+/*
+ * author: Crunchify.com
+ * 
+ */
+ 
+@Controller
+public class ejemploHolaMundo {
+ 
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hola Mundo, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
